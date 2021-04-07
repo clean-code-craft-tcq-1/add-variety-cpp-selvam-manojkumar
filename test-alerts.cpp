13bdf5c1dac3112e581bf345161e56fe4e4fce10
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-04-07 16:54:45 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,3 +6,9 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
+
+TEST_CASE("check and alert through email") {
+	alertToController* emailmsgobj = new alertToController;
+	BatteryCharacter batteryObj = { HI_ACTIVE_COOLING , "Exide" };
+	checkAndAlert(emailmsgobj, batteryObj, 20);
+}
