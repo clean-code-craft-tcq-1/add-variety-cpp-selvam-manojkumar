9cee6fd049dac157a91f487bbc6315a4976ffe7e
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-04-08 11:43:44 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,8 +3,9 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
+
 TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(22, 20, 30) == NORMAL);
 }
 
 TEST_CASE("check and alert through email") {
